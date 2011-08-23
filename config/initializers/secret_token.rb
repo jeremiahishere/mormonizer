e664813b0181aeb2c0f7ea044607dfd95ed8c0b6
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-08-23 09:58:54 -0400

    Created a new rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mormonizer::Application.config.secret_token = '1a01c6ce7fe3c29551017cf449959b5e4950942ae5b159c41bb79d3315ddbb1c63190a963dcec883e10ef3d9c7e8460201f8193bdc233224a17cd7ec62ebe01d'
