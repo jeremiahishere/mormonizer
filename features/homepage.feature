c2fc2250c212b15a26c28acb3e3a0ddc4892096c
Jeremiah Hemphill <jeremiah@cloudpsace.com>
2011-09-01 10:12:19 -0400

    Setup paths, a basic feature, and set the config to run in progress mode.

---
+++ features/homepage.feature
@@ -0,0 +1,6 @@
+Feature: wheeeeee
+
+  Scenario: wheeeeeeI
+    Given I am on the home page
+    When I go to the home page
+    Then I should be on the home page
--- features/support/paths.rb
+++ features/support/paths.rb
@@ -11,6 +11,27 @@ module NavigationHelpers
     when /^the home\s?page$/
       '/'
 
+    # note the weird hack when matching the model
+    # in almost every case, it should match only the plural version of the model
+    # but in the person model, it needs to match people with no plural
+    when /^the (.+?) \- (.+?[s]*)(?:'s)? index page$/     
+      polymorphic_path([$1.to_sym, $2.to_sym])
+
+    when /^the (.+?) \- #{capture_model}(?:'s)? view page for #{capture_model}$/     
+      polymorphic_path([$1.to_sym, model!($3)])    
+
+    when /^the (.+?) \- #{capture_model}(?:'s)? edit page for #{capture_model}$/     
+      polymorphic_path([$1.to_sym, model!($3)], :action => "edit")    
+
+    # due to pickle limitations, we can't use polymorphic path
+    # this is better than manually writing out all the new page paths
+    # but prone to breaking
+    when /^the (.+?) \- (.+?)(?:'s)? new page$/     
+      eval("new_" + $1 + "_" + $2.singularize + "_path")
+
+    when /^the (.+?) \- #{capture_model}(?:'s)? (.+?) page$/  
+      polymorphic_path([$1.to_sym, model!($2)], :action => $3)   
+
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
@@ -20,7 +41,7 @@ module NavigationHelpers
     else
       begin
         page_name =~ /^the (.*) page$/
-        path_components = $1.split(/\s+/)
+          path_components = $1.split(/\s+/)
         self.send(path_components.push('path').join('_').to_sym)
       rescue NoMethodError, ArgumentError
         raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
