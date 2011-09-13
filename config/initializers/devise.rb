de086534ee14e9cb4b5615c047231fa3d317c6f6
Jeremiah Hemphill <jeremiah@cloudpsace.com>
2011-09-13 10:07:16 -0400

    Setup facebook login.  Fixed all the views the web themes generator broke.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,9 @@
 # Use this hook to configure devise mailer, warden hooks and so forth. The first
 # four configuration values can also be set straight in your models.
 Devise.setup do |config|
+  # omniauth facebook login
+  config.omniauth :facebook, '208040305925438', 'b25ca4d7704b37fe2fb8df06b8eac992'
+
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Mormonizer::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
 
   namespace :admin do
+    root :to => "translations#index"
     resources :translations
     resources :users
   end
